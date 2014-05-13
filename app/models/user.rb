8b1bd8431f74e448cd947395176a136d50ff62c8
Sasha Kovryga <sashakovryga@gmail.com>
2014-05-13 14:10:28 +0300

    Cancan_enumerize

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ActiveRecord::Base
+
+	has_many :posts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, 
+         :validatable
+
+  before_create :role_create
+
+  extend Enumerize
+	enumerize :role, in: [:user, :admin, :guest], default: :guest, predicates: true
+
+  protected
+
+  def role_create 
+    self.role = :user
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140512091426) do
+ActiveRecord::Schema.define(version: 20140513065218) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -20,7 +20,10 @@ ActiveRecord::Schema.define(version: 20140512091426) do
     t.string   "life_cycle"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -35,6 +38,7 @@ ActiveRecord::Schema.define(version: 20140512091426) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
